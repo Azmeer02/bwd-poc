5c0864e648fdb32bb3f5cc017a63745c3fb8d147
Syed Azmeer <syedazmeerhaider@gmail.com>
2022-08-11 21:43:06 +0500

    backward functionality & local storage

--- src/tutorial/index.js
+++ src/tutorial/index.js
@@ -1,51 +1,97 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
 import { Progress } from "antd";
+import { RollbackOutlined } from "@ant-design/icons";
+
 import ReactPlayer from "react-player";
+
 import "./index.css";
 
 const Index = () => {
+  const [state, setState] = useState({
+    playing: false,
+    controls: false,
+  });
   const [progress, setProgress] = useState({
     played: 0,
     playedSeconds: 0,
   });
   const [duration, setDuration] = useState();
+
+  const playerRef = useRef(null);
+
   var progressMin = Math.floor((progress.playedSeconds / 60) << 0);
   var progressSec = Math.floor(progress.playedSeconds % 60);
 
   var durationMin = Math.floor((duration / 60) << 0);
   var durationSec = Math.floor(duration % 60);
 
-  const onEnd = () => {
-    console.log("HAHAHAHAHA");
+  const handleOnPlay = () => {
+    if (!state.playing) {
+      setState({ ...state, playing: true });
+    }
+  };
+
+  const handleOnStart = () => {
+    let getCurr = JSON.parse(localStorage.getItem("currentTime"));
+    if (getCurr) {
+      playerRef.current.seekTo(getCurr);
+    } 
+    setState({ ...state, playing: true });
+  };
+
+  const handleOnBack = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
+    handleOnPlay();
   };
 
   return (
-    <div style={{ width: "85%", margin: "0 auto" }}>
-      <div className="progress">
-        <Progress percent={(progress.playedSeconds/duration)*100} />
-      </div>
-      <div className="video-player">
+    <div>
+      <div className="video_player">
         <ReactPlayer
-          className="react-player"
-          url="https://www.youtube.com/watch?v=t0Q2otsqC4I"
-          onEnded={onEnd}
-          width="100%"
-          height="95vh"
-          onProgress={(e) => setProgress(e)}
-          onDuration={(e) => setDuration(e)}
+          className="react_player"
           config={{
             youtube: {
               playerVars: {
                 disablekb: 1,
-                controls: 1,
+                controls: 0,
               },
             },
           }}
+          url="https://www.youtube.com/watch?v=rfTgO9rpqck"
+          ref={playerRef}
+          playing={true}
+          width="100%"
+          height="95vh"
+          onPlay={handleOnPlay}
+          onStart={handleOnStart}
+          onProgress={(e) => {
+            setProgress(e);
+            if (e.playedSeconds > 5)
+              localStorage.setItem(
+                "currentTime",
+                JSON.stringify(Math.round(e.playedSeconds))
+              );
+          }}
+          onDuration={(e) => setDuration(e)}
         />
       </div>
-      <h3>
-        Duration = {progressMin} : {progressSec} / {durationMin} : {durationSec}
-      </h3>
+      <div className="status_container">
+        <div className="duration">
+          <h3>
+            {progressMin} : {progressSec} / {durationMin} : {durationSec}
+          </h3>
+        </div>
+        <div className="backward" onClick={handleOnBack}>
+          <RollbackOutlined />
+        </div>
+        <div className="progress">
+          <Progress
+            percent={Math.round((progress.playedSeconds / duration) * 100)}
+            showInfo={false}
+          />
+        </div>
+      </div>
     </div>
   );
 };
